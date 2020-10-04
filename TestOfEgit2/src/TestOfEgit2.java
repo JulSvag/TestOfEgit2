0f3e311eacfdcc5a7510b07267b3bbaf5ce7d6cf
Julia Svagrovska <jullina@192.168.1.14>
2020-10-04 18:05:39 +0200

    Initial commit of TestOfEgit2 project.

---
+++ TestOfEgit2/src/TestOfEgit2.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author Julia
+ *
+ */
+public class TestOfEgit2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//testing of Egit 2...
+
+	}
+
+}
